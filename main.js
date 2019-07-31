d6771fe2177a0351aca89c5a144e7087acfd3390
Sourire <953810390@qq.com>
2019-07-31 10:52:46 +0800

    snack

--- main.js
+++ main.js
@@ -15,39 +15,65 @@
 
 //food
 !function (window) {
+
     var map = document.querySelector('#map')
 
-    function Food(width, height, color) {
+    var elements = [] //存储food
+
+    function Food(x, y, width, height, color) {
         this.width = width || 20
         this.height = height || 20
         this.color = color
-        this.x = 0
-        this.y = 0
-        
-        this.food = document.createElement('div')
+        this.x = x || 0
+        this.y = y || 0
     }
     //初始化food的效果及位置并显示在map上
     Food.prototype.init = function (map) {
+        removeFoodDiv()
+
+        this.food = document.createElement('div')
         var foodDiv = this.food
-        foodDiv.style.position = 'absolute'
-        foodDiv.style.width = this.width + 'px'
-        foodDiv.style.height = this.height + 'px'
-        foodDiv.style.backgroundColor = this.color
-       
         map.appendChild(foodDiv)
+        elements.push(foodDiv)
+
+        let style={
+            position:'absolute',
+            width:this.width+'px',
+            height:this.height+'px',
+            backgroundColor:this.color
+        }
+        for(let i in style){
+            foodDiv.style[i]=style[i]
+        }
+
+        // foodDiv.style.position = 'absolute'
+        // foodDiv.style.width = this.width + 'px'
+        // foodDiv.style.height = this.height + 'px'
+        // foodDiv.style.backgroundColor = this.color
+
         this.render(map)
     }
-     //产生随机位置
-     Food.prototype.render = function (map) {
-        var x = Random.getRandom(0, map.offsetWidth / this.width) * this.width
-        var y = Random.getRandom(0, map.offsetHeight / this.height) * this.height
-        this.x = x
-        this.y = y
+    //产生随机位置
+    Food.prototype.render = function (map) {
+        this.x = Random.getRandom(0, map.offsetWidth / this.width) * this.width
+        this.y = Random.getRandom(0, map.offsetHeight / this.height) * this.height
         var foodDiv = this.food
         foodDiv.style.left = this.x + 'px'
         foodDiv.style.top = this.y + 'px'
     }
 
-    var snack = new Food(20, 20, 'rgb(153, 236, 57)')
+    var snack = new Food(0, 0, 20, 20, 'rgb(153, 236, 57)')
     snack.init(map)
+
+    //删除food
+    function removeFoodDiv (){
+        for(let i =0;i<elements.length;i++){
+            let ele = elements[i]
+            //删除map中的foodDiv
+            ele.parentNode.removeChild(ele)
+            //删除elements数组中的foodDiv
+            elements.splice(i,1)
+        }
+    }
+
 }(window)
